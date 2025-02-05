76683d4106c1d8ead2cf5a47e961a10b0957b8f5
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-02-05 16:12:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,14 @@
-// $(selector).attrchange(): a call back function for when an attribute changes
-//
-// can return attribute changes for selector
-(function($) {
-  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+(function($, document) {
+    
+      // get tallest tab__content element
+      let height = -1;
 
-  $.fn.attrchange = function(callback) {
-    if (MutationObserver) {
-      var options = {
-        subtree: false,
-        attributes: true
-      };
-
-      var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(e) {
-          callback.call(e.target, e.attributeName);
-        });
-      });
-
-      return this.each(function() {
-        observer.observe(this, options);
-      });
-
-    }
-  }
-})(jQuery);
-
-// when one details opens, close the others
-$('.details-item').attrchange(function(attribute){
-  if(attribute == "open" && $(this).attr("open")) {
-    $(this).siblings(".details-item").removeAttr("open");
-  }
-});
-
-// keyboard: prevent closing the open details to emulate tabs
-$('.details-tab').on("keydown", function(e) {
-  if(e.keyCode == 32 || e.keyCode == 13) {
-    if($(this).parent().attr("open")) {
-      e.preventDefault();
-    }
-  } 
-});
-
-// mouse: prevent closing the open details to emulate tabs
-$('.details-tab').on("click", function(e) {
-  if($(this).parent().attr("open")) {
-    e.preventDefault();
-  }
-});
+		$('.tab__content').each(function() {
+			height = height > $(this).outerHeight() ? height : $(this).outerHeight();
+         $(this).css('position', 'absolute');
+		});
+      
+      // set height of tabs + top offset
+		$('[data-tabs]').css('min-height', height + 40 + 'px');
+   
+}(jQuery, document));
