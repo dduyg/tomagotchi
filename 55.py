303e9b59fae1a224bb8d26be7a3abc0d0a1e226b
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-02-28 13:40:36 +0100

    Update 55.py

--- 55.py
+++ 55.py
@@ -1,13 +1,14 @@
 import random
-
 def magic_8_ball():
     responses = [
-        "Yes, definitely!", "Ask again later.", "No, I don't think so.", "Absolutely!", "Maybe... Try again.", "I wouldn't count on it.",
-        "Yes, but be cautious.", "Don't bet on it.", "My sources say yes.", "Not in a million years."
+        "Yes, definitely!", "Ask again later.", "No, I don't think so.",
+        "Without a doubt.", "Maybe... Try again.", "I wouldn't count on it.",
+        "Yes, but be cautious.", "My sources say yes.", "Signs point to no.",
+        "Reply hazy, try again.", "Better not tell you now."
     ]
     
-    question = input("Ask a yes/no question: ")
-    print("Thinking...")
+    question = input("Ask a yes-or-no question: ")
+    print("Shaking the Magic 8-Ball...\n")
     print("The Magic 8-Ball says:", random.choice(responses))
 
 magic_8_ball()
