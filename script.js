6627a53640505c4ae9105898c65633783b7cb894
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-11-16 01:45:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,82 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.149.0";
+let camera, scene, renderer, clock;
+let uniforms, headMesh, bodyMesh, legMesh, armsMesh, noseMesh;
 
-let camera, scene, renderer, clock, points;
-let uniforms;
-
-let sphere = new THREE.SphereGeometry(7, 50, 50);
+let SCREEN_WIDTH = window.innerWidth;
+			let SCREEN_HEIGHT = window.innerHeight;
+			let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
 
 function init() {
 	const container = document.getElementById("shader");
 
 	clock = new THREE.Clock();
+	camera =  new THREE.PerspectiveCamera( 50, 0.5 * aspect, 1, 10000 );
+	camera.position.z = 3;
 
 	scene = new THREE.Scene();
 
+ const head =  new THREE.SphereGeometry( .2, 320, 320, 160 )
+ 
+  const body =  new THREE.SphereGeometry( .3, 320, 320, 160 )
+  
+   const legs =  new THREE.SphereGeometry( .4, 320, 320, 160 )
+   
+   const arms = new THREE.CylinderGeometry( .01, .01, 1, 132, 132 ); 
+  
+  
+    const nose = new   THREE.ConeGeometry( .1, .8, 132, 132 ); 
+
+
 	uniforms = {
-		uTime: { type: "f", value: 1.0 },
-		uResolution: { type: "v2", value: new THREE.Vector2() },
-		u_progress: {
-			value: 0
-		},
-		uMouse: {
-			value: { x: 0.5, y: 0.5 }
-		}
+		u_time: { type: "f", value: 1.0 },
+		u_resolution: { type: "v2", value: new THREE.Vector2() },
 	};
 
 	const material = new THREE.ShaderMaterial({
+    transparent: true,
+    side: THREE.DoubleSide,
 		uniforms,
 		vertexShader: document.getElementById("vertex").textContent,
 		fragmentShader: document.getElementById("fragment").textContent
 	});
 
-	material.transparent = true;
-	material.depthWrite = false;
+	 headMesh = new THREE.Mesh(head, material);
+	scene.add(headMesh);
+  
+  headMesh.position.y+=.6
+  
+   noseMesh = new THREE.Mesh(nose, material);
+	scene.add(noseMesh);
+  
+  noseMesh.position.y+=.6
+  
+   noseMesh.rotation.x = Math.PI / 2;
+  
+    // noseMesh.rotation.z = Math.PI / 2;
+  
+  
+   bodyMesh = new THREE.Mesh(body, material);
+	scene.add(bodyMesh);
+  
+  bodyMesh.position.y+=.1
+  
+   armsMesh = new THREE.Mesh(arms, material);
+	scene.add(armsMesh);
+  
+  armsMesh.position.y+=.1
+  armsMesh.position.z+=.15
+  
+  armsMesh.rotation.x = Math.PI / 2;
+  
+    armsMesh.rotation.z = Math.PI / 2;
+  
+  
+   legsMesh = new THREE.Mesh(legs, material);
+	scene.add(legsMesh);
+  
+  legsMesh.position.y -=.6
 
-	const pointsGeometry = new THREE.BufferGeometry();
-
-	pointsGeometry.setAttribute(
-		"position",
-		new THREE.BufferAttribute(sphere.attributes.position.array, 3)
-	);
-
-	renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor(0xffffff, 1);
+	renderer = new THREE.WebGLRenderer({alpha: false});
 	renderer.setPixelRatio(window.devicePixelRatio);
-
-	material.uniforms.uResolution.value.x = renderer.domElement.width;
-	material.uniforms.uResolution.value.y = renderer.domElement.height;
-
-	camera = new THREE.PerspectiveCamera(
-		75,
-		renderer.domElement.width / renderer.domElement.height,
-		0.1,
-		100
-	);
-	camera.position.z = 12;
-	camera.position.y = 0;
-	camera.position.x = 0;
-
-	let running = false;
-
-	let finished = function () {
-		running = false;
-	};
-
-	points = new THREE.Points(pointsGeometry, material);
-	scene.add(points);
-	window.addEventListener("mousemove", function (e) {
-		material.uniforms.uMouse.value.x =
-			(event.clientX / window.innerWidth) * 2 - 1;
-		material.uniforms.uMouse.value.y =
-			-(event.clientY / window.innerHeight) * 2 + 1;
-	});
-
-	// window.addEventListener("click", function (e) {
-	// 	if (!running) {
-	// 		running = true;
-	// 		gsap.to(material.uniforms.u_progress, {
-	// 			duration: 6.5,
-	// 			value: material.uniforms.u_progress.value + Math.PI,
-	// 			delay: 0,
-	// 			onComplete: finished
-	// 		});
-	// 	}
-	// });
 
 	container.appendChild(renderer.domElement);
 
@@ -91,21 +86,18 @@ function init() {
 
 function onWindowResize() {
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	uniforms.uResolution.value.x = renderer.domElement.width;
-	uniforms.uResolution.value.y = renderer.domElement.height;
+	uniforms.u_resolution.value.x = renderer.domElement.width;
+	uniforms.u_resolution.value.y = renderer.domElement.height;
 }
 
 function render() {
-	uniforms.uTime.value = clock.getElapsedTime();
+	uniforms.u_time.value = clock.getElapsedTime();
+ 
 	renderer.render(scene, camera);
-
-	if (points) {
-		points.rotation.x += 0.002;
-		points.rotation.z += 0.0002;
-	}
 }
 
 function animate() {
+  
 	render();
 	requestAnimationFrame(animate);
 }
