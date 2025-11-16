768bdd004f2c4ad021c0d1559c5bccaa0db812bf
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-11-16 01:50:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
 let camera, scene, renderer, clock;
-let uniforms, headMesh, bodyMesh, legMesh, armsMesh, noseMesh;
-
-let SCREEN_WIDTH = window.innerWidth;
-			let SCREEN_HEIGHT = window.innerHeight;
-			let aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
+let uniforms;
 
 function init() {
 	const container = document.getElementById("shader");
 
 	clock = new THREE.Clock();
-	camera =  new THREE.PerspectiveCamera( 50, 0.5 * aspect, 1, 10000 );
-	camera.position.z = 3;
+	camera = new THREE.Camera();
+	camera.position.z = 1;
 
 	scene = new THREE.Scene();
 
- const head =  new THREE.SphereGeometry( .2, 320, 320, 160 )
- 
-  const body =  new THREE.SphereGeometry( .3, 320, 320, 160 )
-  
-   const legs =  new THREE.SphereGeometry( .4, 320, 320, 160 )
-   
-   const arms = new THREE.CylinderGeometry( .01, .01, 1, 132, 132 ); 
-  
-  
-    const nose = new   THREE.ConeGeometry( .1, .8, 132, 132 ); 
-
+	const geometry = new THREE.PlaneBufferGeometry(2, 2);
 
 	uniforms = {
 		u_time: { type: "f", value: 1.0 },
@@ -32,50 +18,15 @@ function init() {
 	};
 
 	const material = new THREE.ShaderMaterial({
-    transparent: true,
-    side: THREE.DoubleSide,
 		uniforms,
 		vertexShader: document.getElementById("vertex").textContent,
 		fragmentShader: document.getElementById("fragment").textContent
 	});
 
-	 headMesh = new THREE.Mesh(head, material);
-	scene.add(headMesh);
-  
-  headMesh.position.y+=.6
-  
-   noseMesh = new THREE.Mesh(nose, material);
-	scene.add(noseMesh);
-  
-  noseMesh.position.y+=.6
-  
-   noseMesh.rotation.x = Math.PI / 2;
-  
-    // noseMesh.rotation.z = Math.PI / 2;
-  
-  
-   bodyMesh = new THREE.Mesh(body, material);
-	scene.add(bodyMesh);
-  
-  bodyMesh.position.y+=.1
-  
-   armsMesh = new THREE.Mesh(arms, material);
-	scene.add(armsMesh);
-  
-  armsMesh.position.y+=.1
-  armsMesh.position.z+=.15
-  
-  armsMesh.rotation.x = Math.PI / 2;
-  
-    armsMesh.rotation.z = Math.PI / 2;
-  
-  
-   legsMesh = new THREE.Mesh(legs, material);
-	scene.add(legsMesh);
-  
-  legsMesh.position.y -=.6
+	const mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
 
-	renderer = new THREE.WebGLRenderer({alpha: false});
+	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio(window.devicePixelRatio);
 
 	container.appendChild(renderer.domElement);
@@ -92,12 +43,10 @@ function onWindowResize() {
 
 function render() {
 	uniforms.u_time.value = clock.getElapsedTime();
- 
 	renderer.render(scene, camera);
 }
 
 function animate() {
-  
 	render();
 	requestAnimationFrame(animate);
 }
