3c70df28d315b9610818a9ea6aa14103cf189f78
Duygu Dağdelen <101258339+dduyg@users.noreply.github.com>
2025-11-20 21:06:29 +0100

    Add data/glyph-data.js

---
+++ data/glyph-data.js
@@ -0,0 +1,232 @@
+const GLYPH_DATA = [
+  {
+    "id": "square_f8fafe_20251120_200431_37f09a23",
+    "filename": "square_f8fafe_20251120_200431_37f09a23.png",
+    "original_filename": "inwave1741295081476.png",
+    "shape": "square",
+    "color": {
+      "hex": "f8fafe",
+      "rgb": [
+        248,
+        250,
+        254
+      ],
+      "lab": [
+        98.23,
+        0.05,
+        -2.14
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_6e777e_20251120_200431_359c6908",
+    "filename": "square_6e777e_20251120_200431_359c6908.png",
+    "original_filename": "inwave1741295102421.png",
+    "shape": "square",
+    "color": {
+      "hex": "6e777e",
+      "rgb": [
+        110,
+        119,
+        126
+      ],
+      "lab": [
+        49.51,
+        -1.83,
+        -4.98
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_d1cfd2_20251120_200431_c9aaf841",
+    "filename": "square_d1cfd2_20251120_200431_c9aaf841.png",
+    "original_filename": "inwave1742259532300.png",
+    "shape": "square",
+    "color": {
+      "hex": "d1cfd2",
+      "rgb": [
+        209,
+        207,
+        210
+      ],
+      "lab": [
+        83.36,
+        1.23,
+        -1.24
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_535250_20251120_200431_0bd78487",
+    "filename": "square_535250_20251120_200431_0bd78487.png",
+    "original_filename": "inwave1742259641399.png",
+    "shape": "square",
+    "color": {
+      "hex": "535250",
+      "rgb": [
+        83,
+        82,
+        80
+      ],
+      "lab": [
+        34.91,
+        -0.02,
+        1.31
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_ffffff_20251120_200431_170e7341",
+    "filename": "square_ffffff_20251120_200431_170e7341.png",
+    "original_filename": "inwave1763249160724.png",
+    "shape": "square",
+    "color": {
+      "hex": "ffffff",
+      "rgb": [
+        255,
+        255,
+        255
+      ],
+      "lab": [
+        100.0,
+        -0.0,
+        0.0
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_f5ffff_20251120_200431_6324fa34",
+    "filename": "square_f5ffff_20251120_200431_6324fa34.png",
+    "original_filename": "inwave1763509282753.png",
+    "shape": "square",
+    "color": {
+      "hex": "f5ffff",
+      "rgb": [
+        245,
+        255,
+        255
+      ],
+      "lab": [
+        99.28,
+        -3.27,
+        -1.14
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_c3b0cc_20251120_200431_89293274",
+    "filename": "square_c3b0cc_20251120_200431_89293274.png",
+    "original_filename": "inwave1763509443095.png",
+    "shape": "square",
+    "color": {
+      "hex": "c3b0cc",
+      "rgb": [
+        195,
+        176,
+        204
+      ],
+      "lab": [
+        74.2,
+        11.94,
+        -11.68
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_75a1b6_20251120_200431_b459bc72",
+    "filename": "square_75a1b6_20251120_200431_b459bc72.png",
+    "original_filename": "inwave1763509902662.png",
+    "shape": "square",
+    "color": {
+      "hex": "75a1b6",
+      "rgb": [
+        117,
+        161,
+        182
+      ],
+      "lab": [
+        63.87,
+        -9.54,
+        -15.6
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_7d7084_20251120_200431_fc185928",
+    "filename": "square_7d7084_20251120_200431_fc185928.png",
+    "original_filename": "inwave1763510000210.png",
+    "shape": "square",
+    "color": {
+      "hex": "7d7084",
+      "rgb": [
+        125,
+        112,
+        132
+      ],
+      "lab": [
+        49.03,
+        9.05,
+        -9.13
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  },
+  {
+    "id": "square_241510_20251120_200431_04e74b62",
+    "filename": "square_241510_20251120_200431_04e74b62.png",
+    "original_filename": "inwave1763510867598.png",
+    "shape": "square",
+    "color": {
+      "hex": "241510",
+      "rgb": [
+        36,
+        21,
+        16
+      ],
+      "lab": [
+        8.56,
+        6.88,
+        5.95
+      ]
+    },
+    "timestamp": {
+      "date": "2025-11-20",
+      "time": "20:04:31"
+    }
+  }
+];
